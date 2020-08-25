a8e50d5a53630a0adaa90c0e18b0d7e53b6d4675
Everton Oliveira <everton_ace@hotmail.com>
2020-08-25 20:26:20 -0300

    Release 25-08-2020

--- PPTRANControlesWebApp/Startup.cs
+++ PPTRANControlesWebApp/Startup.cs
@@ -13,6 +13,9 @@ using PPTRANControlesWebApp.Areas.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using PPTRANControlesWebApp.Areas.Identity.Data;
 using PPTRANControlesWebApp.Areas.Identity.Models;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace PPTRANControlesWebApp
 {
@@ -75,6 +78,13 @@ namespace PPTRANControlesWebApp
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            RequestLocalizationOptions localizationOptions = new RequestLocalizationOptions
+            {
+                SupportedCultures = new List<CultureInfo> { new CultureInfo("pt-BR")},
+                SupportedUICultures = new List<CultureInfo> { new CultureInfo("pt-BR")},
+                DefaultRequestCulture = new RequestCulture("pt-BR")
+            };
 
             app.UseStaticFiles();
             app.UseAuthentication();
